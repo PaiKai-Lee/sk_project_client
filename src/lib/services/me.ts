fd1e799f1ada159e6ebcc918d8d65e2fae158fd7
Kai <b790718@gmail.com>
2023-05-21 15:34:34 +0800

    新增: 新增me頁面，頭像上傳功能

---
+++ src/lib/services/me.ts
@@ -0,0 +1,36 @@
+import { fetchBackend } from '../utils';
+
+class Me {
+	async getMyProfile() {
+		const [result, err] = await fetchBackend('get', '/api/me');
+		if (err) {
+			return [null, err];
+		}
+		const url = import.meta.env.VITE_API_URL;
+		const { avatar } = result;
+		const avatarSrc = url + avatar;
+		return [
+			{
+				...result,
+				avatar: avatarSrc
+			},
+			null
+		];
+	}
+
+	getMyRecords() {
+		return fetchBackend('get', '/api/me/records');
+	}
+
+	async uplaodAvatar(formData: FormData) {
+		return fetchBackend('post', '/api/me/avatar', {
+			data: formData,
+			headers: { 'Content-type': 'multipart/form-data' }
+		});
+	}
+	async updateMyProfile() {}
+}
+
+const meService = new Me();
+
+export default meService;
