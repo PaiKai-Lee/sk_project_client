3e02038ba6fb4324c7406f6719520efde2e1fa39
Kai <b790718@gmail.com>
2023-05-28 23:54:53 +0800

    新增: 新增Accordion component; 調整: 專案預計項目改為Accordion顯示

---
+++ src/lib/components/common/accordion/Accordion.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+  export let colapse = false;
+
+  const activeComponentId = writable(null);
+
+  setContext('colapse', colapse);
+  setContext('active', activeComponentId);
+</script>
+
+<div class="w-full p-2 text-xl border-gray-300 text-black bg-white rounded-lg shadow-lg">
+  <slot />
+</div>
--- src/routes/(app)/progress/+page.svelte
+++ src/routes/(app)/progress/+page.svelte
@@ -1,37 +1,56 @@
+<script>
+  import PageTitle from '$lib/components/common/PageTitle.svelte';
+  import { Accordion, AccordionItem } from '$lib/components/common/accordion';
+
+  const items = [
+    {
+      title: '管理者頁面',
+      content: [
+        { name: '檢視所有使用者', isDone: true },
+        { name: '權限管理user不能使用', isDone: true },
+        { name: '重製使用者密碼', isDone: true },
+        { name: '新增刪除使用者', isDone: true },
+        { name: '修改使用者權限', isDone: true }
+      ]
+    },
+    { title: 'Line bot整合', content: [] },
+    {
+      title: '主畫面',
+      content: [
+        { name: '網站Banner', isDone: true },
+        { name: '顯示公司夥伴', isDone: true },
+        { name: '個人圖片彈出簡介', isDone: false }
+      ]
+    },
+    {
+      title: '使用者設定',
+      content: [
+        { name: '密碼變更彈窗', isDone: true },
+        { name: '密碼變更功能', isDone: true }
+      ]
+    }
+  ];
+</script>
+
 <svelte:head>
   <title>專案進度</title>
 </svelte:head>
 
-<h1 class="text-4xl font-bold text-shadow text-center my-4">預計項目</h1>
+<PageTitle>預計項目</PageTitle>
+
 <div class="mx-auto w-1/3 max-w-1/2">
-  <div class="container">
-    <div class="w-full p-2 my-4 shadow-md shadow-zinc-400 rounded-xl">
-      <p class="text-lg font-bold">管理者頁面</p>
-      <ul class="list-disc list-inside">
-        <li class="line-through">檢視所有使用者</li>
-        <li class="line-through">權限管理user不能使用</li>
-        <li class="line-through">重製使用者密碼</li>
-        <li class="line-through">新增刪除使用者</li>
-        <li class="line-through">修改使用者權限</li>
-      </ul>
-    </div>
-    <div class="w-full p-2 my-4 shadow-md shadow-zinc-400 rounded-xl">
-      <p class="text-lg font-bold">Line bot整合</p>
-    </div>
-    <div class="w-full p-2 my-4 shadow-md shadow-zinc-400 rounded-xl">
-      <p class="text-lg font-bold">主畫面</p>
-      <ul class="list-disc list-inside">
-        <li class="line-through">網站Banner</li>
-        <li>顯示公司夥伴grid?</li>
-        <li>個人圖片彈出簡介?</li>
-      </ul>
-    </div>
-    <div class="w-full p-2 my-4 shadow-md shadow-zinc-400 rounded-xl">
-      <p class="text-lg font-bold">使用者設定</p>
-      <ul class="list-disc list-inside">
-        <li class="line-through">密碼變更彈窗</li>
-        <li class="line-through">密碼變更功能</li>
-      </ul>
-    </div>
-  </div>
+  <Accordion>
+    {#each items as item, idx (idx)}
+      <AccordionItem>
+        <svelte:fragment slot="title">{item.title}</svelte:fragment>
+        <ul slot="content">
+          {#each item.content as contentItem, idx (idx)}
+            <li class="list-disc {contentItem.isDone && 'line-through'}">
+              {contentItem.name}
+            </li>
+          {/each}
+        </ul>
+      </AccordionItem>
+    {/each}
+  </Accordion>
 </div>
